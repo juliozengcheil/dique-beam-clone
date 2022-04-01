e0ddd909fa08040cd5056e67bbd08a40ec316864
julio-zeng-cheil <julio.zeng@cheil.com>
2022-04-01 15:30:34 -0600

    fix a lint warning

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,25 +1,24 @@
 import NextAuth from "next-auth";
-import GitHub from 'next-auth/providers/github'
+import GitHub from "next-auth/providers/github";
 
-import { serverEnv } from '../../../env/server'
+import { serverEnv } from "../../../env/server";
 
-export default (req, res) =>
-    NextAuth(req, res, {
-        providers: [
-            GitHub({
-                clientId: process.env.GITHUB_ID,
-                clientSecret: process.env.GITHUB_SECRET,
-            }),
-        ],
-        debug: process.env.NODE_ENV === "development",
-        secret: serverEnv.NEXTAUTH_SECRET,
-        jwt: {
-            secret: process.env.JWT_SECRET,
-        },
-        callbacks: {
-            async redirect(url, baseUrl) {
-                return "/";
-                // return url;
-            },
-        },
-    });
+export default NextAuth({
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  secret: serverEnv.NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+  callbacks: {
+    async redirect(url, baseUrl) {
+      return "/";
+      // return url;
+    },
+  },
+});
